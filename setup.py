bbb8c82ab9ad3fd563c9a429f73b1b23ce45f888
Abraham Ameh <abrahamfreela@gmail.com>
2020-05-08 11:27:24 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="finance_ml",
+    name="pyfinance",
     version='0.1',
-    description='utility library for finance',
-    author='jjakimoto',
-    author_email='f.j.akimoto@gmail.com',
+    description='utility for finance',
+    author='dgde',
+    author_email='ajene.ameh.abraham@gmail.com,
     packages=find_packages(),
-    py_modeuls=["finance_ml"]
+    py_modeuls=["pyfinance"]
 )
